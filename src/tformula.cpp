42883db40f14123bdbd84dff68d595a50847c7a4
Alyona <ryabova.alena99@gmail.com>
2018-12-07 01:23:40 +0300

    1

---
+++ src/tformula.cpp
@@ -0,0 +1,264 @@
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include "TStack.h"
+#include "TFormula.h"
+
+
+TFormula :: TFormula (char *form)                            // конструктор
+{
+	int size = strlen(form);
+	if (size>=MaxLen) throw -1;
+	for (int i=0; i<size; i++)
+		Formula[i] = form[i];
+}
+
+int TFormula :: FormulaChecker(int Brackets[],int size)     // проверка корректности скобок
+{
+	TStack st(size);
+	for (int i=0; i<size; i++)
+	{
+		if (Formula[i]=='(') st.Put(i);
+		if (Formula[i]==')') st.Get();
+	}
+	if (st.IsEmpty()) return 1;
+	else return 0;
+}
+
+int TFormula :: FormulaConverter()                          // преобразование в постфиксную форму
+{
+	int size = 0;
+	int ind = 0;
+	while (Formula[ind] != '=') { size++; ind++;}
+	TStack st(size);
+
+	int Index = 0; //Длина постфиксной формы
+	int Priority = -1;		//Приоритет операции
+	int PriorityStackOp = -1; //Приоритет последней операции в стеке
+
+	for (int i=0; i<size; i++)
+	{
+		if ((Formula[i] >= '0')&&(Formula[i] <= '9')||(Formula[i] == '.'))
+		{
+			PostfixForm[Index] = Formula [i];
+			Index++;
+			std::cout<<Formula [i];
+		}
+		if ((Formula[i] == '+')||(Formula[i] == '-')||(Formula[i] == '*')||(Formula[i] == '/')
+			||(Formula[i] == '(')||(Formula[i] == ')'))
+		{
+			switch (Formula[i])
+			{
+				case '+': 
+				{
+					PostfixForm[Index] = ' ';
+					std::cout<<PostfixForm[Index];
+					Index++;
+					Priority = 2;
+					if (Priority<=PriorityStackOp)									//если меньше или равен
+					{
+						while (!st.IsEmpty())
+						{
+							PostfixForm[Index] = st.Get();
+							std::cout<<PostfixForm[Index];
+							Index++;
+							PostfixForm[Index] = ' ';
+							std::cout<<PostfixForm[Index];
+							Index++;
+						}
+					}
+					if ((st.IsEmpty())||(Priority>PriorityStackOp))					//если больше
+					{
+						st.Put('+');	
+						PriorityStackOp = Priority;
+					}
+					break;
+				}
+				case '-':
+				{
+					PostfixForm[Index] = ' ';
+					std::cout<<PostfixForm[Index];
+					Index++;
+					Priority = 2;
+					if (Priority<=PriorityStackOp)
+					{
+						while (!st.IsEmpty())
+						{
+							PostfixForm[Index] = st.Get();
+							std::cout<<PostfixForm[Index];
+							Index++;
+							PostfixForm[Index] = ' ';
+							std::cout<<PostfixForm[Index];
+							Index++;
+						}
+					}
+					if ((st.IsEmpty())||(Priority>PriorityStackOp))
+					{
+						st.Put('-');	
+						PriorityStackOp = Priority;
+					}
+					break;
+				}
+				case '*': 
+				{
+					PostfixForm[Index] = ' ';
+					std::cout<<PostfixForm[Index];
+					Index++;
+					Priority = 3;
+					if (Priority<=PriorityStackOp)
+					{
+						while (!st.IsEmpty())
+						{
+							PostfixForm[Index] = st.Get();
+							std::cout<<PostfixForm[Index];
+							Index++;
+							PostfixForm[Index] = ' ';
+							std::cout<<PostfixForm[Index];
+							Index++;
+						}
+					}
+					if ((st.IsEmpty())||(Priority>PriorityStackOp))
+					{
+						st.Put('*');	
+						PriorityStackOp = Priority;
+					}
+					break;
+				}
+				case '/' :
+				{
+					PostfixForm[Index] = ' ';
+					std::cout<<PostfixForm[Index];
+					Index++;
+					Priority = 3;
+					if (Priority<=PriorityStackOp)
+					{
+						while (!st.IsEmpty())
+						{
+							PostfixForm[Index] = st.Get();
+							std::cout<<PostfixForm[Index];
+							Index++;
+							PostfixForm[Index] = ' ';
+							std::cout<<PostfixForm[Index];
+							Index++;
+						}
+					}
+					if ((st.IsEmpty())||(Priority>PriorityStackOp))
+					{
+						st.Put('/');	
+						PriorityStackOp = Priority;
+					}
+					break;
+				}
+				case '(' :
+				{
+					Priority = 0;
+					PriorityStackOp = Priority;
+					st.Put('(');
+					break;
+				}
+				case ')' :
+				{
+					PostfixForm[Index] = ' ';
+					std::cout<<PostfixForm[Index];
+					Index++;
+					Priority = 1;
+					PriorityStackOp = Priority;
+					while (st.TopElem()!='(')
+						{
+							PostfixForm[Index] = st.Get();
+							std::cout<<PostfixForm[Index];
+							Index++;
+							PostfixForm[Index] = ' ';
+							std::cout<<PostfixForm[Index];
+							Index++;
+						}
+					st.Get();
+					break;
+				}
+			}
+		}
+		if (!((Formula[i] >= '0')&&(Formula[i] <= '9')||(Formula[i] == '.'))&&(!((Formula[i] == '+')||(Formula[i] == '-')||(Formula[i] == '*')||(Formula[i] == '/')
+			||(Formula[i] == '(')||(Formula[i] == ')'))))
+			throw -1;
+	}
+	while(!st.IsEmpty()) {
+		PostfixForm[Index] = ' ';
+		std::cout<<PostfixForm[Index];
+		Index++;
+		PostfixForm[Index] = st.Get();
+		std::cout<<PostfixForm[Index];
+		Index++;
+	}
+	PostfixForm[Index] = ' ';
+
+	std::cout<<std::endl;
+	return 1;
+}
+
+double TFormula :: FormulaCalculator()                      // вычисление формулы
+{
+	double str [MaxLen] = {0};
+	char y [50];
+
+	double result = 0;
+	FormulaConverter();
+
+	int ind = 0; //индекс в str
+	int i = 0; //индекс в PostfixForm
+	while (PostfixForm[i] != '\0')
+	{
+		if ((PostfixForm[i] >= '0')&&(PostfixForm[i] <= '9')) 
+		{
+			int j = 0;
+			while (PostfixForm[i] != ' ') {
+				y[j] = PostfixForm[i];
+				i++; j++;
+			}
+			y[j] = 0;
+			str [ind] = atof(y);
+			ind++;
+		}
+		switch (PostfixForm[i])
+		{
+			case '+' :
+				{
+					double top_el = str[--ind]; //запоминаем элемент на вершине
+					str[ind] = 0; //обнуляем
+					result = str [--ind] + top_el; //складываем с предыдущим
+					str[ind] = result; //кладем результат
+					ind++; //перемещаем индекс на cлед позицию
+					break;
+				}
+			case '-' :
+				{
+					double top_el = str[--ind];
+					str[ind] = 0; 
+					result = str [--ind] - top_el;
+					str[ind] = result;
+					ind++;
+					break;
+				}
+			case '*' :
+				{
+					double top_el = str[--ind];
+					str[ind] = 0; 
+					result = str [--ind] *  top_el;
+					str[ind] = result;
+					ind++;
+					break;
+				}
+			case '/' :
+				{
+					double top_el = str[--ind];
+					str[ind] = 0; 
+					result = str [--ind] / top_el;
+					str[ind] = result;
+					ind++;
+					break;
+				}
+		}
+		i++;
+	}
+
+	return result;
+}
---
+++ src/tstack.cpp
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <iostream>
+#include "tstack.h"
+
+
+/*
+	PTElem pMem;      // память для СД
+	int MemSize;      // размер памяти для СД
+	int DataCount;    // количество элементов в СД
+	TMemType MemType; // режим управления памятью
+
+	#define DefMemSize   25  // размер памяти по умолчанию
+
+	#define DataEmpty  -101  // СД пуста
+	#define DataFull   -102  // СД переполнена
+	#define DataNoMem  -103  // нет памяти
+
+	#define DataOK   0
+	#define DataErr -1
+	int RetCode; // Код завершения
+*/
+
+TStack :: TStack (int size)
+{
+	if (size<0) throw -1;
+	if (size==0) 
+		this -> pMem = NULL;
+	else 
+		this -> pMem = new TElem [size];
+	this -> MemSize = size;
+	this-> DataCount = 0;
+	top = -1;
+}
+
+void TStack::Put(const TData& Val)  {
+	if (IsFull()) 
+		SetRetCode(DataFull);
+	if (pMem==NULL)
+		SetRetCode(DataNoMem);
+	if (this->RetCode==DataOK) {
+		top++;
+		pMem[top] = Val;
+		DataCount++;
+	}
+}
+
+TData TStack::Get() {
+	if (IsEmpty())
+		SetRetCode(DataEmpty);
+	if (pMem==NULL)
+		SetRetCode(DataNoMem);
+	if (this->RetCode==DataOK) {
+		DataCount--;
+		return pMem[top--];
+	}
+}
+
+TData TStack::TopElem() {
+	if (IsEmpty()) 
+	{
+		SetRetCode(DataEmpty);
+		throw -1;
+	}
+	if (pMem==NULL) 
+	{
+		SetRetCode(DataNoMem);
+		throw -1;
+	}
+	return pMem[top];
+}
+
+
+int TStack:: IsValid() {
+	return GetRetCode();
+}
+
+void TStack::Print() {
+	if (IsEmpty()) std::cout<<"-"; 
+	for (int i = 0; i < DataCount; i++)
+		std::cout<<(char)pMem[i]<<" ";
+	std::cout<<std::endl;
+}
+
