56bccd01c13a54eebd1861026858a1cd2732333b
black8cherry <art8evd@gmail.com>
2018-12-14 14:50:08 +0300

    a1

--- include/tformula.h
+++ include/tformula.h
@@ -5,9 +5,12 @@ class TFormula
    private:
      char Formula[MaxLen];     // исходная формула
      char PostfixForm[MaxLen]; // постфиксная форма
+	 int formulaLength;
+	 int postfixFormulaLength = 0;
    public:
      TFormula(char *form);                            // конструктор
-     int FormulaChecker(int Brackets[],int size);     // проверка корректности скобок
+     int FormulaChecker();     // проверка корректности скобок
      int FormulaConverter();                          // преобразование в постфиксную форму
      double FormulaCalculator();                      // вычисление формулы
+	 int Priority(char sign);
 };
